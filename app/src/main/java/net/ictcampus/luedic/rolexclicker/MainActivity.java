0f2cee514c846b1dadf20c96902b30527d27d8af
Claudio Luedi <luedic@ict-campus.net>
2018-05-25 16:11:49 +0200

    first app commit

--- app/src/main/java/net/ictcampus/luedic/rolexclicker/MainActivity.java
+++ app/src/main/java/net/ictcampus/luedic/rolexclicker/MainActivity.java
@@ -2,12 +2,33 @@ package net.ictcampus.luedic.rolexclicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            TextView tv = (TextView) findViewById(R.id.textView);
+            long score = 0;
+            @Override
+            public void onClick(View v) {
+                score+=1;
+                tv.setText("Score: "+score);
+            }
+        });
+        Button btnShop = (Button) findViewById(R.id.buttonshop);
+        btnShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
     }
 }
+
